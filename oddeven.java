47cefe542773b05e256055c4cc7270ebe6823105
tham12 <47381690+tham12@users.noreply.github.com>
2019-02-06 15:28:09 +0530

    Create oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.Scanner; 
+class oddeven
+{
+   public static void main(String args[])
+   {
+		int a;
+		Scanner in = new Scanner(System.in);
+		a = in.nextInt();
+		if(a>0)
+		{
+			if (a % 2 == 0)
+			{
+				System.out.println("Even");
+			}
+			else
+			{
+				System.out.println("Odd");
+			}
+			}
+		else
+		{
+			System.out.println("Invalid");
+		}
+   }
+}
