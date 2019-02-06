3a607505732d61198a430a8e5024038b4670934c
tham12 <47381690+tham12@users.noreply.github.com>
2019-02-06 16:20:29 +0530

    Update VovCons.java

--- VovCons.java
+++ VovCons.java
@@ -1,26 +1,39 @@
 import java.util.Scanner;
-class VowCons
- {
-  public static void main(String args[])
+ class VowCons
+ 
+{
+  
+ public static void main(String args[])
+ 
   {
-     System.out.println("Enter a character :");
+     
    	Scanner sc = new Scanner(System.in);
-     char ch = sc.next().charAt(0);
-      if(ch == 'a'|| ch == 'e'|| ch == 'i' ||ch == 'o' ||ch == 'u')
-    {
-        System.out.println("vowel");
+     
+	 char ch = sc.next().charAt(0);
+     
+ if(ch == 'a'|| ch == 'e'|| ch == 'i' ||ch == 'o' ||ch == 'u')
+    
+ 	 {
+        
+	 System.out.println("Vowel");
   
 	  }
 else
-      {
-      if(ch>='a'&&ch<='z'||ch>='A'&&ch<='Z')
-    {  
-     System.out.println("consonant");
-   }
+     
+	 {
+
+if(ch>='a'&&ch<='z'||ch>='A'&&ch<='Z')
+
+  {  
+     System.out.println(" Consonant");
+ 
+     }
 else
 {
-System.out.println("default");
+System.out.println("Invalid");
+ 
   }
+
 }
 }
 }
