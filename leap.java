baaae2597e5ad5c7aa472c2ab7e444c6909608f7
tham12 <47381690+tham12@users.noreply.github.com>
2019-02-27 15:38:59 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+ class leap {
+
+    public static void main(String[] args) {
+
+    	int year;
+    	Scanner scan = new Scanner(System.in);
+    	year = scan.nextInt();
+    	scan.close();
+        boolean isLeap = false;
+
+        if(year % 4 == 0)
+        {
+            if( year % 100 == 0)
+            {
+                if ( year % 400 == 0)
+                    isLeap = true;
+                else
+                    isLeap = false;
+            }
+            else
+                isLeap = true;
+        }
+        else {
+            isLeap = false;
+        }
+
+        if(isLeap==true)
+            System.out.println(year + "");
+        else
+            System.out.println(year + "");
+    }
+}
