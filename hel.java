69a702045fb15305f8edf4198137963d731b2687
tham12 <47381690+tham12@users.noreply.github.com>
2019-02-27 16:15:47 +0530

    Create hel.java

---
+++ hel.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ class hel
+{
+    public static void main(String args[])
+    {
+       int N;
+       Scanner scan = new Scanner(System.in);
+       N = scan.nextInt();
+        for (int i = 0; i < N; i++) 
+        {
+            System.out.println("hello");
+        }
+    }
+}
